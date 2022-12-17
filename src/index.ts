d2e1f9243d0c5d2c0cc6581d8a0ce3a3659b0ef0
holmista <tornikebuchukuri@redberry.ge>
2022-12-18 00:50:20 +0400

    chore: include port in env

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use("/images", imageRouter);
 app.use("/info", resourceRouter);
 
-app.listen(3000, () => console.log("listening to port 3000"));
+app.listen(process.env.PORT, () => console.log("listening to port 3000"));
 
 ["uncaughtException", "unhandledRejection"].forEach((event) => {
   process.on(event, (error) => {
