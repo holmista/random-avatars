d4c6b20ac0d4e6a4facbc10386f3ca513724d83f
holmista <tornikebuchukuri@redberry.ge>
2022-12-27 16:35:31 +0400

    feat: add middleware to resource routes

--- src/routes/resourceRoutes.ts
+++ src/routes/resourceRoutes.ts
@@ -6,11 +6,16 @@ import {
   approveResource,
   rejectResource,
 } from "../controllers/resourceController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/available-resources", getResources);
-router.get("/unapproved-resources", getUnapprovedResources);
-router.get("/unapproved-resources/:resource", getUnapprovedResource);
-router.get("/approve-resource/:resource", approveResource);
-router.get("/reject-resource/:resource", rejectResource);
+router.get("/unapproved-resources", authMiddleware, getUnapprovedResources);
+router.get(
+  "/unapproved-resources/:resource",
+  authMiddleware,
+  getUnapprovedResource
+);
+router.get("/approve-resource/:resource", authMiddleware, approveResource);
+router.get("/reject-resource/:resource", authMiddleware, rejectResource);
 export default router;
