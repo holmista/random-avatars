75fbda0a599c7ad7dd63c820b7af0f283043d9d5
holmista <tornikebuchukuri@redberry.ge>
2022-12-31 21:33:43 +0400

    feat: me route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 const router = Router();
 import { login } from "../controllers/authController.js";
 import validateData from "../middlewares/loginMiddleware.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 router.post("/login", validateData, login);
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({ message: "authorized" });
+});
 
 export default router;
